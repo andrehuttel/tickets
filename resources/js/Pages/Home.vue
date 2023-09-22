49c0e1131c8529c7c2fed1e00556c949067d7ff5
André <andreluizhuttel@gmail.com>
2023-09-22 16:10:01 -0300

    change the data fetched in home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -113,13 +113,13 @@ defineExpose({ primaryColor, secondColor, storeTitle });
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                     </svg>
-                                                    Abertura: {{ formatTime(event.date_opening) }} 
+                                                    Abertura: {{ event.place_open_hour }} 
                                                 </span>
                                                 <span class="text-gray-400 inline-flex items-center leading-none text-xs" :style="{ color: primaryColor ? primaryColor : '' }">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                     </svg>
-                                                    Inicio: {{ formatTime(event.date) }}
+                                                    Inicio: {{ event.start_hour }}
                                                 </span>
                                             </div>
                                             <div class="flex items-center flex-wrap">
@@ -128,7 +128,7 @@ defineExpose({ primaryColor, secondColor, storeTitle });
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
                                                 </svg>
-                                                {{ event.place }}
+                                                {{ event.place_name }}
                                                 </span>
                                             </div>
                                             <!-- <div class="flex items-center flex-wrap ">
